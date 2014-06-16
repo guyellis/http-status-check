295a4075e31b2a4996c6ecdea3e6aeeb2073e15c
Guy Ellis <wildfiction@gmail.com>
2014-06-16 14:35:13 -0700

    Updated for response headers

--- index.js
+++ index.js
@@ -13,15 +13,32 @@ function siteCheck(site) {
 	}
 	request.get(options, function(error, response, body){
 		if(response.statusCode !== site.expectedStatus) {
-			console.log('Failed check for ' + site.name +
-					'. Expected HTTP status of ' + site.expectedStatus +
-					' and got ' + response.statusCode + '.');
+			console.log('X ' + site.name + ' failed. Expected HTTP status of ' +
+					site.expectedStatus + ' and got ' + response.statusCode + '.');
 		} else {
 			if(site.responseHeaders) {
-				// TODO: Check response headers
-				console.log('responseHeaders: ', site.responseHeaders);
+				var accumulatedHeaderFails = [];
+				for (var header in site.responseHeaders) {
+					if (site.responseHeaders.hasOwnProperty(header)) {
+						var actualHeaderValue = response.headers[header];
+						var expectedHeaderValue = site.responseHeaders[header];
+						if(actualHeaderValue !== expectedHeaderValue) {
+							accumulatedHeaderFails.push(
+									'Expected header \'' + header + '\' to be ' +
+											expectedHeaderValue + ' but instead it was ' +
+											actualHeaderValue
+							);
+						}
+					}
+				}
+				if(accumulatedHeaderFails.length === 0) {
+					console.log('_ ' + site.name + ' working as expected.');
+				} else {
+					console.log('X ' + site.name + ' returned the expected status but some of the headers did not match:');
+					console.log(accumulatedHeaderFails);
+				}
 			} else {
-				console.log(site.name + ' working as expected.');
+				console.log('_ ' + site.name + ' working as expected.');
 			}
 		}
 	});
@@ -29,7 +46,7 @@ function siteCheck(site) {
 
 function run(filename) {
 	var file = require(filename);
-	console.log(file);
+	// console.log(file);
 	for(var i=0, n=file.sites.length; i<n; i++) {
 		siteCheck(file.sites[i]);
 	}
