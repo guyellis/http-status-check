1ec6234c2d6880c002f233a91db4d4be4d8a63ea
Guy Ellis <wildfiction@gmail.com>
2014-06-17 14:19:50 -0700

    Added another unit test

--- test/code_uriCheck.js
+++ test/code_uriCheck.js
@@ -20,7 +20,7 @@ describe('code/uriCheck/', function() {
 	describe('startsWith()', function () {
 		var testString = 'this is a string';
 		var startsWith = uriCheck.__get__('startsWith');
-		
+
 		it('should return true if the string starts with one of the array items', function (done) {
 			var protocols = ['this', 'is'];
 			var result = startsWith(testString,protocols);
@@ -41,9 +41,9 @@ describe('code/uriCheck/', function() {
 			var site = {
 				disabled: true
 			};
-			var eachSiteCallCount = 0;
+			var checkUriCallCount = 0;
 			uriCheck.__set__('checkUri', function(site,callback){
-				eachSiteCallCount++;
+				checkUriCallCount++;
 			});
 			var writeResultCallCount = 0;
 			uriCheck.__set__('outAdapter',{
@@ -55,10 +55,48 @@ describe('code/uriCheck/', function() {
 			uriCheck.checkUri(site, function(err) {
 				errValue = err;
 			});
-			eachSiteCallCount.should.equal(0);
+			checkUriCallCount.should.equal(0);
 			writeResultCallCount.should.equal(1);
 			should.not.exist(errValue);
 
+			done();
+		});
+
+		it('should call writeResult with fail if request.get() returns an error', function (done) {
+			var site = {
+				"name": "Unit Test",
+				"expectedStatus": 200,
+				// missing http in front of requestedUrl will be added by call to checkUri
+				"requestUrl": "www.unittest.com",
+				"requestHeaders": {
+					"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0"
+				}
+			};
+
+			var requestGetCallCount = 0;
+			uriCheck.__set__('request', {
+				get: function(options, callback) {
+					requestGetCallCount++;
+					return callback({Error: new Error('Unit Test Error')});
+				}
+			});
+			var writeResultCallCount = 0;
+			var writeResultResult = '';
+			uriCheck.__set__('outAdapter',{
+				writeResult: function(result, uri) {
+					writeResultResult = result;
+					writeResultCallCount++;
+				}
+			});
+			var errValue = {};
+			uriCheck.checkUri(site, function(err) {
+				errValue = err;
+			});
+			requestGetCallCount.should.equal(1);
+			writeResultCallCount.should.equal(1);
+			should.not.exist(errValue);
+			writeResultResult.should.equal('fail');
+			
 			done();
 		});
 	});
