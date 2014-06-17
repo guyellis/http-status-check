471c0f804c67db56fa8cc466ab91158a7a775564
Guy Ellis <wildfiction@gmail.com>
2014-06-17 15:33:50 -0700

    Added more unit tests.

--- test/outAdapters_consoleAdapter.js
+++ test/outAdapters_consoleAdapter.js
@@ -8,13 +8,51 @@ var rewire = require('rewire');
 var outAdapter = rewire('../outAdapters/consoleAdapter');
 
 describe('outAdapters/consoleAdapter/', function() {
+
+	describe('organizeErrors()', function () {
+		it('should put object properties into an array', function (done) {
+			var organizeErrors = outAdapter.__get__('organizeErrors');
+			var errors = {
+				one: 'one',
+				two: 'two'
+			};
+			var result = organizeErrors(errors);
+			should.exist(result);
+			Array.isArray(result).should.equal(true);
+			result.length.should.equal(2);
+			done();
+		});
+	});
+
+	describe('organizeErrors()', function () {
+		it('should put string into an array', function (done) {
+			var organizeErrors = outAdapter.__get__('organizeErrors');
+			var errors = 'one';
+			var result = organizeErrors(errors);
+			should.exist(result);
+			Array.isArray(result).should.equal(true);
+			result.length.should.equal(1);
+			result[0].should.equal('one');
+			done();
+		});
+	});
+
 	describe('writeResult()', function () {
 		it('should increment success counter when successful', function (done) {
-			var failHits = outAdapter.__get__('failHits');
 			outAdapter.writeResult('success', {});
-			failHits.should.equal(0);
+			var failHits = outAdapter.__get__('failHits');
 			var successHits = outAdapter.__get__('successHits');
-			successHits.should.equal(1);
+			successHits.should.be.above(0);
+			done();
+		});
+	});
+
+	describe('writeResult()', function () {
+		it('should increment fail counter when unsuccessful', function (done) {
+			outAdapter.writeResult('fail', {});
+			var failHits = outAdapter.__get__('failHits');
+			var successHits = outAdapter.__get__('successHits');
+			failHits.should.be.above(0);
 			done();
 		});
 	});
