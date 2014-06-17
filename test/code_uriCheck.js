2660af30b9de53038f16d283317bffc4f867c4ae
Guy Ellis <wildfiction@gmail.com>
2014-06-17 13:38:34 -0700

    Added init and siteCheck() unit tests

--- test/code_uriCheck.js
+++ test/code_uriCheck.js
@@ -5,12 +5,43 @@
 
 var should = require('chai').should();
 var rewire = require('rewire');
-var manager = require('../code/uriCheck');
+var uriCheck = rewire('../code/uriCheck');
 
 describe('code/uriCheck/', function() {
-	describe('()', function () {
-		it('', function (done) {
+	describe('init()', function () {
+		it('sets the outAdapter', function (done) {
+			uriCheck.init({test:true});
+			var outAdapter = uriCheck.__get__("outAdapter");
+			outAdapter.test.should.equal(true);
 			done();
 		});
 	});
+
+	describe('siteCheck()', function () {
+		it('terminates early if the site is disabled', function (done) {
+			var site = {
+				disabled: true
+			};
+			var eachSiteCallCount = 0;
+			uriCheck.__set__('eachSite', function(site,callback){
+				eachSiteCallCount++;
+			});
+			var writeResultCallCount = 0;
+			uriCheck.__set__('outAdapter',{
+				writeResult: function() {
+					writeResultCallCount++;
+				}
+			});
+			var errValue = {};
+			uriCheck.siteCheck(site, function(err) {
+				errValue = err;
+			});
+			eachSiteCallCount.should.equal(0);
+			writeResultCallCount.should.equal(1);
+			should.not.exist(errValue);
+
+			done();
+		});
+	});
+
 });
