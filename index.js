11d12f793c96df7757de22c1969ff0b35a634263
Guy Ellis <wildfiction@gmail.com>
2014-06-17 12:11:22 -0700

    Added test coverage for index

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 "use strict";
 
-var request = require('request');
-var _ = require('lodash');
 var manager = require('./code/manager');
 var inAdapter = require('./inAdapters/fileAdapter');
 var outAdapter = require('./outAdapters/consoleAdapter');
 
-function main() {
+var main = function() {
 	var runData = inAdapter.getRunData();
 	manager.run(runData, outAdapter);
 }
 
 main();
+
+module.exports = {
+	main: main
+};
--- test/test_index.js
+++ test/test_index.js
@@ -5,11 +5,26 @@
 
 var should = require('chai').should();
 var rewire = require('rewire');
-var index = require('../index');
+var index = rewire('../index');
 
 describe('index/', function() {
-	describe('()', function () {
-		it('', function (done) {
+	describe('main()', function () {
+		it('should call getRunData() and manager.run()', function (done) {
+			var getRunDataCalled = false;
+			var runCalled = false;
+			index.__set__('inAdapter', {
+				getRunData: function() {
+					getRunDataCalled = true;
+				}
+			});
+			index.__set__('manager', {
+				run: function() {
+					runCalled = true;
+				}
+			});
+			index.main();
+			getRunDataCalled.should.be.equal(true);
+			runCalled.should.be.equal(true);
 			done();
 		});
 	});
