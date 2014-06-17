300051c35f3ee2a0065622852227acd2375067d9
Guy Ellis <wildfiction@gmail.com>
2014-06-17 14:56:09 -0700

    Added more unit tests.

--- test/inAdapters_fileAdapter.js
+++ test/inAdapters_fileAdapter.js
@@ -5,11 +5,27 @@
 
 var should = require('chai').should();
 var rewire = require('rewire');
-var inAdapter = require('../inAdapters/fileAdapter');
+var inAdapter = rewire('../inAdapters/fileAdapter');
 
 describe('inAdapters/fileAdapter/', function() {
-	describe('()', function () {
-		it('', function (done) {
+	inAdapter.__set__('require', function(filename) {
+		return {unit: 'test'};
+	});
+
+	describe('getRunData()', function () {
+		it('should return checksites.json data if exists', function (done) {
+			var existsSyncCallCount = 0;
+			inAdapter.__set__('fs', {
+				existsSync: function(filename) {
+					existsSyncCallCount++;
+					return true;
+				}
+			});
+			var runData = inAdapter.getRunData();
+			existsSyncCallCount.should.equal(1);
+			console.log(runData);
+			should.exist(runData.unit);
+			runData.unit.should.equal('test');
 			done();
 		});
 	});
