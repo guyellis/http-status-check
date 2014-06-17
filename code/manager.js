3d59cac94a41ce2306869e3dad08a22a226d6c9e
Guy Ellis <wildfiction@gmail.com>
2014-06-17 12:31:10 -0700

    Added unit test

--- code/manager.js
+++ code/manager.js
@@ -1,6 +1,5 @@
 "use strict";
 
-var request = require('request');
 var uriCheck = require('./uriCheck');
 var _ = require('lodash');
 var async = require('async');
--- test/code_manager.js
+++ test/code_manager.js
@@ -5,11 +5,30 @@
 
 var should = require('chai').should();
 var rewire = require('rewire');
-var manager = require('../code/manager');
+var manager = rewire('../code/manager');
 
 describe('code/manager/', function() {
-	describe('()', function () {
-		it('', function (done) {
+	describe('run()', function () {
+		it('uses 3 as the default concurrentRequests if not supplied', function (done) {
+			var initCalled = false;
+			manager.__set__('uriCheck', {
+				init: function(outAdapter) {
+					initCalled = true;
+				}
+			});
+			var expandInputCalled = false;
+			manager.__set__('expandInput', function() {
+				expandInputCalled = true;
+			});
+			var concurrentRequests = 0;
+			manager.__set__('iterateURLs', function(concurrentRequestsParam, sites){
+				concurrentRequests = concurrentRequestsParam;
+			});
+
+			manager.run({});
+			concurrentRequests.should.equal(3);
+			initCalled.should.equal(true);
+			expandInputCalled.should.equal(true);
 			done();
 		});
 	});
