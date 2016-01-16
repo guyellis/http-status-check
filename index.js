1041d8c12862f7dfaefc7ffeab04e5b555286fae
Guy Ellis <wildfiction@gmail.com>
2016-01-16 08:39:39 -0700

    Update dependencies lodash 3 > 4, nodemailer 1 > 2. Set Node to latest 5.4.1 in Travis and package.json

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var settings = require('./package.json');
 var inAdapter = require(settings.inAdapter.type);
 var outAdapter = require('./lib/outAdapters/consoleAdapter');
 
-var verbose = _.contains(process.argv.slice(2), '--verbose');
+var verbose = _.includes(process.argv.slice(2), '--verbose');
 var main = function(verbose) {
   var sites = inAdapter.getRunData();
   manager.run(sites, settings, outAdapter, verbose);
